cb85830efadf2c730c1865e84c0d363bc40ac9e4
John Brunton <john_brunton@hotmail.co.uk>
2020-06-30 15:56:39 +0100

    define async main() function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 
-try {  
+async function main() {
   const kbldPath = await tc.downloadTool('https://github.com/k14s/kbld/releases/download/v0.23.0/kbld-linux-amd64');  
   const cachedPath = await tc.cacheFile(kbldPath, 'kbld', '0.23.0');
   core.addPath(cachedPath);
-} catch (error) {
-  core.setFailed(error.message);
 }
+
+(async function() {
+  try {
+    await main();
+  } catch(e) {
+    ore.setFailed(e.message);
+  }
+})()
